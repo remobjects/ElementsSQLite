54057241ecb396dcc57a861dd4b805a615fd2945
marc hoffman <mh@remobjects.com>
2022-05-09 09:35:51 -0400

    Build fore .NETStandard2.0, added AssemblyInfo

---
+++ AssemblyInfo.pas
@@ -0,0 +1,23 @@
+﻿namespace RemObjects.Elements.RTL.SQLite;
+
+{$IF ECHOES}
+uses
+  System.Reflection,
+  System.Runtime.CompilerServices,
+  System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RemObjects Elements SQLite3 Wrapper Library")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("RemObjects Software")]
+[assembly: AssemblyProduct("RemObjects Elements")]
+[assembly: AssemblyCopyright("Copyright RemObjects Software, LLC 2015-")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyKeyName("RemObjectsSoftware")]
+{$ENDIF}
+
+end.
